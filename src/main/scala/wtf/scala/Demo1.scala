cada6a15940d6240d0aa9203f99bf6f64dabde1b
Dmitry.K <steklopod@gmail.com>
2018-03-26 23:37:32 +0300

    Imports

---
+++ src/main/scala/wtf/scala/Demo1.scala
@@ -0,0 +1,14 @@
+package wtf.scala
+
+
+class Demo1 {
+  val property = 29
+  def propertyDef = 29
+}
+
+
+
+object Demo1Test extends App{
+  val age = new Demo1.property
+  println(age)
+}
---
+++ src/main/scala/wtf/scala/imports/Demo.scala
@@ -0,0 +1,14 @@
+package wtf.scala.imports
+
+import java.io.StringWriter
+import java.util.{ArrayList, HashMap, TreeMap}
+
+/**
+  * Импорт java-классов
+  */
+class Demo {
+  val x = new ArrayList[String]()
+  val y = new HashMap[String, TreeMap[String, String]]()
+  val z = new StringWriter()
+}
+
