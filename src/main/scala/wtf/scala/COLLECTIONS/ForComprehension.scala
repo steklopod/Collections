a5331b9b76790577c2885870ca9d9e10e0ba0afb
Dmitry.K <steklopod@gmail.com>
2018-04-08 14:28:48 +0300

    YIELD

--- src/main/scala/wtf/scala/COLLECTIONS/ForComprehension.scala
+++ src/main/scala/wtf/scala/COLLECTIONS/ForComprehension.scala
@@ -11,4 +11,23 @@ object ForExample extends App {
   for (n <- names) {
     println("NAME: " + n + "\n")
   }
+
+//  ANOTHER VARIANT:
+  println("ANOTHER VARIANT: \n")
+  (0 until 10).foreach(a => println(a))
+
+  // 1. YIELD:
+  println("YIELD [capitalize First letter]: \n")
+  val capNames = for( e <- names) yield  e.capitalize
+
+  capNames.foreach(println(_))
+
+  // 2. YIELD:
+  val length = for ( e <- names) yield {
+    e.length
+  }
+  length.foreach(println(_))
+
+
+
 }
