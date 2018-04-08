34be34b8ada4dc0578f101ac09ce53674f9f38f8
Dmitry.K <steklopod@gmail.com>
2018-04-08 10:47:42 +0300

    companion object

--- src/main/scala/wtf/scala/OOP/object_example/Companion.scala
+++ src/main/scala/wtf/scala/OOP/object_example/Companion.scala
@@ -1,6 +1,23 @@
 package wtf.scala.OOP.object_example
 
-class Companion {
+class Companion(val name: String, val age: Int)
 
+//Java/static class members == Scala/companion object, companion module
+object Companion {
+  val MAX_AGE = 99
+  val MAX_NAME_LENGTH = 1024
+
+  def apply(name: String, age: Int) = new Companion(name, age)
 }
+
+object Demo extends App{
+
+  val comp0 = new Companion("Dima", 29)
+
+  val comp1 = Companion.apply("Dima", 29)
+
+  val comp2 = Companion("Dima", 29)
+}
+
+
 
