b0f8b85152a2ed6f6ca04161b058657bf1917f12
Dmitry.K <steklopod@gmail.com>
2018-04-08 13:48:54 +0300

    Collections Types (кратко)

--- src/main/scala/wtf/scala/COLLECTIONS/Basics.scala
+++ src/main/scala/wtf/scala/COLLECTIONS/Basics.scala
@@ -4,13 +4,32 @@ class Basics {
 
 }
 
-object Lists extends App {
+object CollectionTypesEx extends App {
 
+  //  LIST
   val numbers = List(1, 2, 3, 4)
-  println(numbers)
+  println(">LIST: \n" + numbers)
 
-  val sets =  Set(1,2,3)
-  println(sets)
+  //  SET
+  val sets = Set(1, 2, 3)
+  println(">SET: \n" + sets)
+
+  //  MAP:
+  println(">MAP: \n" + Map(1 -> 2))
+
+  //  ARRAY
+  println(">ARRAY: \n" + Array(1, 2))
+
+  //  CORTEZ
+  val hostPort = ("localhost", 80)
+
+  println(">CORTEZ: \n" + hostPort)
+  println("-only first element: " + hostPort._1)
+  println("-second element: " + hostPort._2)
+
+
 }
+
+
 
 
