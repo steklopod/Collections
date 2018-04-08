b270e78caa311d8fe2f383a8f27c9ae1ff5000e7
Dmitry.K <steklopod@gmail.com>
2018-04-08 22:37:40 +0300

    Option Matching

--- src/main/scala/wtf/scala/FUNCTIONS/Matching.scala
+++ src/main/scala/wtf/scala/FUNCTIONS/Matching.scala
@@ -5,4 +5,54 @@ class Matching {
 
 }
 
-//TODO - стр. 57
+object MatcherTest extends App {
+
+  def matchInt(num: Int): String = {
+    num match {
+      case 1 => "one"
+      case 2 => "two"
+      case _ => "many" //все остальное
+    }
+  }
+
+  println(matchInt(5))
+
+
+  /**
+    * Сопоставление с образцом
+    */
+  def matchIntCondition(num: Int): String = {
+    num match {
+      case i if i == 1 => "один"
+      case i if i == 2 => "два"
+      case _ => "много"
+    }
+  }
+
+  println(matchIntCondition(2))
+}
+
+object CaseClassMatching extends App {
+  val opt: Option[Int] = Some(4)
+
+  opt match {
+    case Some(n) if n > 5 => println(s"Мой номер $n > 5")
+    case Some(_) => println("Число меньше чем 5")
+    case None => println("No numbers")
+  }
+
+  println(opt(1))
+  println(opt(6))
+
+}
+
+object Twice {
+  def unapply(num: Int ): Option[Int] = if (num % 2 == 0)
+    Some(num /2 ) else
+    
+}
+//TODO - стр. 65
+
+
+
+
